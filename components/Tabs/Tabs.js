6b8f0f7e5608e143b7a63b4531a6c33ec2de9fac
focused220 <42449262+focused220@users.noreply.github.com>
2019-02-07 19:45:04 -0800

    Second Commit

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,7 +14,7 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.tabItem.addEventListener('click', (event) => this.select());
+    this.element.addEventListener('click', (event) => this.select());
   };
 
   select() {
@@ -24,14 +24,14 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach(link => 
-      {link.getElementsByClassName('.tabs-link-selected').removeAttribute('.tabs-links-selected');    
-    })
+      {link.classList.remove('tabs-link-selected');
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    this.element.setAttribute('class', 'tabs-link-selected');
+    this.tabItem.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link    
-    this.element.select();
+    this.tabItem.select();
   } 
    
 }
